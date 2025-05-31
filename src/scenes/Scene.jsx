dff2cb711d3bd769206a72d8fa91746b49c8eaf5
Aashutosh <aashutoshdahal22@gmail.com>
2025-05-31 14:59:56 +0545

    charcter moves on road only

--- src/scenes/Scene.jsx
+++ src/scenes/Scene.jsx
@@ -1,6 +1,6 @@
 import { Canvas } from "@react-three/fiber";
-import React, { useState } from "react";
-import {Text, OrbitControls } from "@react-three/drei";
+import React, { useRef, useState } from "react";
+import { Text, OrbitControls } from "@react-three/drei";
 import CameraController from "./utils/CameraController";
 import { useCamera, CameraProvider } from "./context/CameraContext";
 
@@ -12,7 +12,7 @@ import Kumari_road from "./environments/Kumari_road";
 import Large_road from "./environments/Large_road";
 import Cars from "./environments/Cars";
 import Realcharacter from "./environments/Realchar";
-import NewTrees from "./environments/Trees"
+import NewTrees from "./environments/Trees";
 // import BirdFlock from "./environments/BirdFlock";
 
 //college imports
@@ -39,6 +39,12 @@ import Londonblock from "./college/UK";
 import SkillSSD from "./college/SkillBlockStudentService";
 const Scene = () => {
   const { activeCamera, setActiveCamera } = useCamera();
+    const skillRoadRef = useRef();
+  const kumariRoadRef = useRef();
+  const largeRoadRef = useRef();
+  
+  // Combine all road refs into an array
+  const allRoadRefs = [skillRoadRef, kumariRoadRef, largeRoadRef];
 
   return (
     <>
@@ -57,7 +63,6 @@ const Scene = () => {
             console.log("Camera move complete. Free to move now!")
           }
         />
-
         {/* <Text
           position={[-170, 0, -60]}
           rotation={[0,1.3,0]}
@@ -69,79 +74,78 @@ const Scene = () => {
           textAlign="center"
           font="/fonts/Bebas_Neue/BebasNeue-Regular.ttf"
         > */}
-          SKILL{'\n'}BLOCK
+        SKILL{"\n"}BLOCK
         {/* </Text> */}
-
         <OrbitControls enablePan={true} />
         {/* <ambientLight intensity={0.5}/> */}
-
         <LightWithHelper />
-
         {/* <directionalLight position={[10, 20, 10]} intensity={5} /> */}
-
         {/* uncomment these for environment */}
-
         {/* <Tree count={15} /> */}
         {/* <CustomClouds /> */}
         {/* <BirdFlock /> */}
         {/* <Hills /> */}
-        <NewTrees position={[110, -35.5, 29.2]} rotation ={[0,2.9,0]}/>
-        <NewTrees position={[105, -35.5, 39.2]} rotation ={[0,2.9,0]}/>
-        <NewTrees position={[133, -35.5, 35.6]} rotation ={[0,2.9,0]}/>
-        <NewTrees position={[128, -35.5, 45.6]} rotation ={[0,2.9,0]}/>
-        <NewTrees position={[158, -35.5, 42.9]} rotation ={[0,2.9,0]}/>
-        <NewTrees position={[151, -35.5, 52.6]} rotation ={[0,2.9,0]}/>
-        <Valley 
-        position={[-90, -61.8, -250]} rotation= {[0,-0.3,0]} scale={[4.5,3,3]}
+        <NewTrees position={[110, -35.5, 29.2]} rotation={[0, 2.9, 0]} />
+        <NewTrees position={[105, -35.5, 39.2]} rotation={[0, 2.9, 0]} />
+        <NewTrees position={[133, -35.5, 35.6]} rotation={[0, 2.9, 0]} />
+        <NewTrees position={[128, -35.5, 45.6]} rotation={[0, 2.9, 0]} />
+        <NewTrees position={[158, -35.5, 42.9]} rotation={[0, 2.9, 0]} />
+        <NewTrees position={[151, -35.5, 52.6]} rotation={[0, 2.9, 0]} />
+        <Valley
+          position={[-90, -61.8, -250]}
+          rotation={[0, -0.3, 0]}
+          scale={[4.5, 3, 3]}
         />
-
-        <Valley 
-        position={[-140, -61.8, 550]} rotation= {[0,3,0]} scale={[4.5,3,3]}
+        <Valley
+          position={[-140, -61.8, 550]}
+          rotation={[0, 3, 0]}
+          scale={[4.5, 3, 3]}
         />
-
-        <Cars />
+        {/* <Cars /> */}
         <Skill_road
           position={[11, -34.3, 20.2]}
           rotation={[0, -0.318, 0]}
           scale={[380, 100, 480]}
+          ref={skillRoadRef}
         />
         <Kumari_road
           position={[64, -104.38, -446]}
           rotation={[0, 2.79, 0]}
           scale={[30, 20, 25]}
+          ref={kumariRoadRef}
         />
         <Large_road
           position={[70, -35.34, -72]}
           scale={[32, 20, 35]}
           rotation={[0, -0.33, 0]}
+          ref={largeRoadRef}
         />
         {/* displaying colleges for the main scene */}
-        <Kumari 
-        position={[-0, -41.9, -154]} 
-        rotation={[0, 4.4, 0]} 
-        scale={[2,2,2]}
+        <Kumari
+          position={[-0, -41.9, -154]}
+          rotation={[0, 4.4, 0]}
+          scale={[2, 2, 2]}
         />
-
         <Skill
           position={[-119.7, -36.5, -3]}
           rotation={[0, -0.3, 0]}
-          scale={[2,2,2]}
+          scale={[2, 2, 2]}
           setActiveCamera={setActiveCamera}
         />
         <SkillSSD
           position={[-115.3, -38.5, -55]}
           rotation={[0, -0.3, 0]}
-          scale={[2,2,2]}
-          />
+          scale={[2, 2, 2]}
+        />
         <Alumni
           position={[-55, -36, -20]}
           rotation={[0, -0.3, 0]}
-          scale={[2,2,2]}
+          scale={[2, 2, 2]}
         />
         <Britian
           position={[19, -36.6, -62]}
           rotation={[0, 1.27, 0]}
-          scale={[2,2,2]}
+          scale={[2, 2, 2]}
         />
         {/* <Chautari
           position={[-15, -50.7, 104]}
@@ -162,12 +166,12 @@ const Scene = () => {
         <Canteen
           position={[-92, -40.35, 146]}
           rotation={[0, -0.3, 0]}
-          scale={[2,2,2]}
+          scale={[2, 2, 2]}
         />
         <Londonblock
           position={[-35, -36, 225]}
           rotation={[0, 2.89, 0]}
-          scale={[2,2,2]}
+          scale={[2, 2, 2]}
         />
         <IT
           position={[-128, -33.3, 212]}
@@ -187,13 +191,9 @@ const Scene = () => {
         <BaskeballCourt
           position={[-121, -37.35, 177]}
           rotation={[0, -0.32, 0]}
-          scale={[2,2,2]}
+          scale={[2, 2, 2]}
         />
-
-        
-
-
-        <CharacterController />
+        <CharacterController roadRefs={allRoadRefs} />
         <mesh
           rotation={[-Math.PI / 2, 0, 2.8]}
           position={[-70, -50, 60]}
